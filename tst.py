49c6f33e2fae7e205525a2cca624f24af30fbf54
delivey <deliveydev@gmail.com>
2022-12-26 22:51:13 +0200

    Clean negative contrasting function

--- tst.py
+++ tst.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageColor, ImageEnhance
+from helpers import GetIntensityByte, ClampToByte
 
 og = Image.open("./256.png")
 pixels = og.load()
@@ -16,29 +17,6 @@ else:
     multiply = 1
     divide = 1
 
-def ClampToByte(x): 
-    if x > 255:
-        return 255
-    elif x < 0:
-        return 0
-    return int(x)
-
-rgbTable = []
-for i in range(256**2):
-    rgbTable.append(0)
-
-# Returns: A value in the range 0 to 255 inclusive.
-def GetIntensityByte(rgb):
-    return (7471 * rgb[2] + 38470 * rgb[1] + 19595 * rgb[0]) >> 16
-
-if divide == 100:
-    for intensity in range(256):
-        shift = (intensity - 127 + brightness) * multiply / divide + 127 - intensity;
-
-        for col in range(256):
-            index = (intensity * 256) + col
-            rgbTable[index] = ClampToByte(col + shift)
-
 for i in range(og.size[0]):
     for j in range(og.size[1]):
         colors = pixels[i, j][0:3]
@@ -49,14 +27,10 @@ for i in range(og.size[0]):
             else:
                 og.putpixel((i, j), (255, 255, 255))
         elif divide == 100:
-            inten = GetIntensityByte(colors)
-
-            shiftIndex = inten * 256
-
-            R = rgbTable[shiftIndex + colors[0]]
-            G = rgbTable[shiftIndex + colors[1]]
-            B = rgbTable[shiftIndex + colors[2]]
-
+            shift = (intensity - 127 + brightness) * multiply / divide + 127 - intensity
+            R = ClampToByte(colors[0] + shift)
+            G = ClampToByte(colors[1] + shift) 
+            B = ClampToByte(colors[2] + shift)
             og.putpixel((i, j), (R, G, B))
 
 og.show()
