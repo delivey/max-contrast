38af7e070657e0f6de58e6cb374a125b59f881e9
delivey <deliveydev@gmail.com>
2022-12-26 15:43:05 +0200

    Basic contrast 100 working

--- tst.py
+++ tst.py
@@ -3,13 +3,32 @@ from PIL import Image, ImageColor, ImageEnhance
 og = Image.open("./256.png")
 pixels = og.load()
 
+brightness = 0
+contrast = 100
+
+if contrast < 0:
+    multiply = contrast + 100
+    divide = 100
+elif contrast > 0:
+    multiply = 100
+    divide = 100 - contrast
+else:
+    multiply = 1
+    divide = 1
+
+
+# Returns: A value in the range 0 to 255 inclusive.
+def GetIntensityByte(rgb):
+    return (7471 * rgb[2] + 38470 * rgb[1] + 19595 * rgb[0]) >> 16
+
 for i in range(og.size[0]):  # for every pixel:
     for j in range(og.size[1]):
         colors = pixels[i, j][0:3]  # r g b
 
-        if (sum(colors) / 3) < 128:
+        if GetIntensityByte(colors) < 128:
             og.putpixel((i, j), (0, 0, 0))
         else:
             og.putpixel((i, j), (255, 255, 255))
 
 og.show()
+
